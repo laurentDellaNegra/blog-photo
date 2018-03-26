20093330316f03364df8fa98b73557b9d5db1a72
Laurent Della-Negra <laurent.dellanegra@gmail.com>
2018-03-26 21:04:30 +0200

    Add prod url

--- src/app/admin/album-detail/album-detail.component.ts
+++ src/app/admin/album-detail/album-detail.component.ts
@@ -5,6 +5,13 @@ import { AngularFireStorage } from 'angularfire2/storage';
 
 import { Album } from '../../model/Album';
 
+interface Image {
+  path: string;
+  filename: string;
+  downloadUrl?: string;
+  $key?: string;
+}
+
 @Component({
   selector: 'app-album-detail',
   templateUrl: './album-detail.component.html',
@@ -15,6 +22,7 @@ export class AlbumDetailComponent implements OnInit {
   album: Album;
   uploadPercent: Observable<number>;
   downloadURL: Observable<string>;
+  imageList: Observable<Image[]>;
 
   constructor(
     private route: ActivatedRoute,
@@ -28,6 +36,12 @@ export class AlbumDetailComponent implements OnInit {
   }
 
   uploadFile(event) {
+    // create a random id
+    const randomId = Math.random().toString(36).substring(2);
+
+    // create a reference to the storage bucket location
+    const ref = this.storage.ref(randomId);
+
     const file = event.target.files[0];
     const filePath = 'name-your-file-path-here';
     const task = this.storage.upload(filePath, file);
@@ -36,6 +50,10 @@ export class AlbumDetailComponent implements OnInit {
     this.uploadPercent = task.percentageChanges();
     // get notified when the download URL is available
     this.downloadURL = task.downloadURL();
+  }
+
+  save() {
+
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
     authDomain: 'blog-photo-c92df.firebaseapp.com',
     databaseURL: 'https://blog-photo-c92df.firebaseio.com',
     projectId: 'blog-photo-c92df',
-    storageBucket: '',
+    storageBucket: 'gs://blog-photo-c92df.appspot.com/',
     messagingSenderId: '217094971948'
   }
 };
